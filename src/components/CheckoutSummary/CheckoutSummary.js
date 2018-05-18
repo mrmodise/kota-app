d1c527b1a167ed70e0e08212dee8e89ab7b0da23
Mr Modise <modisemorebodi@gmail.com>
2018-05-18 20:48:42 +0200

    adding checkout functionality

---
+++ src/components/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const checkoutSummary = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+checkoutSummary.propTypes = {
+    
+};
+
+export default checkoutSummary;
