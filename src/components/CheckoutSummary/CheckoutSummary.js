776eac7c82754e4218b7c0c88dd59ed38b0b29eb
Mr Modise <modisemorebodi@gmail.com>
2018-05-26 01:18:03 +0200

    adding more functionality to checktout summary

--- src/components/CheckoutSummary/CheckoutSummary.js
+++ src/components/CheckoutSummary/CheckoutSummary.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import Kota from '../Kota/Kota';
+import Button from '../UI/Button/Button';
+import classes from './CheckoutSummary.css';
 
 const checkoutSummary = props => {
     return (
-        <div>
+        <div className={classes.CheckoutSummary}>
+            <h1>You can never go wrong with ZA's number one Kota</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Kota ingredients={props.ingredients}/>
+            </div>
+
+            <Button btnType={"danger"} clicked={""}>Cancel</Button>
+            <Button btnType={"success"} clicked={""}>Continue</Button>
 
         </div>
     );
-};
-
-checkoutSummary.propTypes = {
-    
 };
 
 export default checkoutSummary;
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import CheckoutSummary from '../../components/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
+    state = {
+        ingredients: {
+            lettuce: 1,
+            meat: 1,
+            cheese: 1,
+            bacon: 1
+        }
+    };
+
     render() {
         return (
             <div>
-                
+                <CheckoutSummary ingredients={this.state.ingredients}/>
             </div>
         );
     }
 }
-
-Checkout.propTypes = {};
 
 export default Checkout;
